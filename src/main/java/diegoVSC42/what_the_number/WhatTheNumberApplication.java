dd98843272b709cc9283013bf12c77904c87aca3
Diego Vieira <2001.vieira.diego@gmail.com>
2024-12-12 11:27:38 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/diegoVSC42/what_the_number/WhatTheNumberApplication.java
@@ -0,0 +1,13 @@
+package diegoVSC42.what_the_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatTheNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diegoVSC42/what_the_number/WhatTheNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package diegoVSC42.what_the_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTheNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
