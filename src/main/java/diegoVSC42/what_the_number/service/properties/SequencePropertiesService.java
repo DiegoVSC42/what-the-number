b05cc85e685a3d5f37d46160695e9b7c2471924b
Diego Vieira <2001.vieira.diego@gmail.com>
2024-12-19 12:14:27 -0300

    fix: correct logic for calculating Fibonacci sequence

--- src/main/java/diegoVSC42/what_the_number/service/properties/SequencePropertiesService.java
+++ src/main/java/diegoVSC42/what_the_number/service/properties/SequencePropertiesService.java
@@ -9,79 +9,79 @@ import org.springframework.stereotype.Service;
 public class SequencePropertiesService {
     public FibonacciSequence calculateFibonacciSequence(Long number) {
 
-        long nthFibonacciTerm = 1;
+        Long nthFibonacciTerm = null;
         Long position = null;
         boolean isInFibonacci = false;
 
-        long f1 = 1;
+        long f1 = 0;
         long f2 = 1;
-        long fn = 1;
+        long fn = 0;
 
-        if(number > 2){
-            for(long i = 2; i <= number; i++) {
-
-                nthFibonacciTerm = fn;
-
-                if (fn == number){
-                    isInFibonacci = true;
-                    position = i;
-                }
-
+        if (number > 3) {
+            for (long i = 1; i <= number; i++) {
                 fn = f1 + f2;
-
                 f2 = f1;
                 f1 = fn;
 
+                if(i == number){
+                    nthFibonacciTerm = fn;
+                }
 
+                if (fn == number) {
+                    isInFibonacci = true;
+                    position = i;
+                    nthFibonacciTerm = fn;
+                }
             }
-        }else if(number == 1){
-            position = 1L;
+        } else {
             isInFibonacci = true;
-        }else if(number == 2){
-            position = 3L;
-            isInFibonacci = true;
+            position = number+1;
+            if(number != 0){
+                nthFibonacciTerm = number-1;
+            }
         }
-        return new FibonacciSequence(nthFibonacciTerm,position,isInFibonacci);
+
+        return new FibonacciSequence(nthFibonacciTerm, position, isInFibonacci);
     }
 
     public TriangularSequence calculateTriangularSequence(Long number) {
 
-        long nthTriangularTerm = (number*(number+1))/2;
+        long nthTriangularTerm = (number * (number + 1)) / 2;
         Long position = null;
         boolean isInTriangular = false;
 
         long triangularFactor;
 
-        for(long i = 1; i <= number; i++) {
-            triangularFactor = (i*(i+1))/2;
-            if(triangularFactor == number){
+        for (long i = 1; i <= number; i++) {
+            triangularFactor = (i * (i + 1)) / 2;
+            if (triangularFactor == number) {
                 isInTriangular = true;
                 position = i;
             }
         }
 
-        return new TriangularSequence(nthTriangularTerm,position,isInTriangular);
+        return new TriangularSequence(nthTriangularTerm, position, isInTriangular);
 
     }
 
     public SquareSequence calculateSquareSequence(Long number) {
-        long nthSquareTerm = number*number;
+        long nthSquareTerm = number * number;
         Long position = null;
         boolean isInSquare = false;
 
-        if(number > 1){
-            for(long i = 0; i <= number/2; i++) {
-                long square = (i*i);
-                if(square == number){
+        if (number > 1) {
+            for (long i = 0; i <= number / 2; i++) {
+                long square = (i * i);
+                if (square == number) {
                     isInSquare = true;
                     position = i;
                 }
             }
-        }else{
+        } else {
             position = number;
             isInSquare = true;
         }
-        return new SquareSequence(nthSquareTerm,position,isInSquare);
+        return new SquareSequence(nthSquareTerm, position, isInSquare);
 
     }
 }
