cc92a89e75c291ea362042e91227cc3d58619209
Diego Vieira <2001.vieira.diego@gmail.com>
2025-01-22 18:04:39 -0300

    Added frontend deploy on cors config

--- src/main/java/diegoVSC42/what_the_number/config/CorsConfig.java
+++ src/main/java/diegoVSC42/what_the_number/config/CorsConfig.java
@@ -9,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:4200")
-			.allowedMethods("GET")
-			.allowedHeaders("*");
+				.allowedOrigins("http://localhost:4200", "https://what-the-number-frontend.vercel.app/")
+				.allowedMethods("GET")
+				.allowedHeaders("*");
 	}
 }
